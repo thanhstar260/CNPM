e8ca606fbee04e543ef1ca2ece6aadb710119368
Dinh Quan <ntdquan2003@gmail.com>
2023-06-07 12:31:53 +0700

    reup main screen

---
+++ BookRentalManagement/app/src/main/java/com/kwan/bookrentalmanagement/Main_Screen.java
@@ -0,0 +1,13 @@
+package com.kwan.bookrentalmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main_Screen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+}
