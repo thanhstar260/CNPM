d952c7c7828caf4f4d3f107f4486b4fbafb1a03b
thanhstar <thanhstar260@gmail.com>
2023-06-16 12:12:45 +0700

    fix error

--- bookrental_web/src/components/layouts/productlisting/ProductListing.js
+++ bookrental_web/src/components/layouts/productlisting/ProductListing.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import "./productlistingcard.style.css";
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./productlisting.style.css";
+import ProductListingCard from "../../cards/product-listing-card/ProductListingCard";
+import { BookData } from "../../../util/BookData";
 
-const ProductListingCard = ({bookData}) => {
-    return (
-        <div className="product-listing-card">
-            <div className="product-listing-img-container">
-                <img src={bookData.book_url} alt="product-listing-image" className="product-listing-image"/>
+const ProductListing = () => {
+    return(
+        <div className="product-listing-container">
+            <div className="sub-product-container">
+                <a href="#" className="product-link">Sách mới</a>
+                <div className="listing-container">
+                    {BookData.slice(0,5).map((book) => (
+                        <ProductListingCard bookData={book} />
+                    ))}
+                </div>
             </div>
-            <div className="product-listing-details-container">
-                <h3>{bookData.book_name}</h3>
-                <p className="author-name">{bookData.author_name}</p>
-                <p className="pricing">{bookData.price}</p>
-                
+
+            <div className="sub-product-container">
+                <a href="#" className="product-link">Sách hot</a>
+                <div className="listing-container">
+                    {BookData.slice(0,5).map((book) => (
+                        <ProductListingCard bookData={book} />
+                    ))}
+                </div>
             </div>
-            <div className="card-btn-container">
-                <Link to={`/book-details/${bookData.id}`} className="product-listing-button">Thêm vào giỏ hàng</Link>
+
+            <div className="sub-product-container">
+                <a href="#" className="product-link">Sách phổ biến</a>
+                <div className="listing-container">
+                    {BookData.slice(0,5).map((book) => (
+                        <ProductListingCard bookData={book} />
+                    ))}
+                </div>
             </div>
+            
         </div>
     )
 }
-export default ProductListingCard;
+
+export default ProductListing;
