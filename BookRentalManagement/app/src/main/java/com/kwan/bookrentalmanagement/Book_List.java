56b1a66fc7f8c2fd04dbe777cfb513ffa793debb
shiraori39 <lightning2904@gmail.com>
2023-06-18 21:05:42 +0700

    navigation

--- BookRentalManagement/app/src/main/java/com/kwan/bookrentalmanagement/Book_List.java
+++ BookRentalManagement/app/src/main/java/com/kwan/bookrentalmanagement/Book_List.java
@@ -1,19 +1,33 @@
 package com.kwan.bookrentalmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Book_List extends AppCompatActivity {
 
     private SearchView searchView;
-
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_list);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Book_List.this, Add_Booklist.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- BookRentalManagement/app/src/main/java/com/kwan/bookrentalmanagement/Main_Screen.java
+++ BookRentalManagement/app/src/main/java/com/kwan/bookrentalmanagement/Main_Screen.java
@@ -1,15 +1,68 @@
 package com.kwan.bookrentalmanagement;
 
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import android.database.sqlite.SQLiteDatabase;
 
-public class Main_Screen extends AppCompatActivity {
+public class Main_Screen extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnBook;
+    private Button btnCustomer;
+    private Button btnReports;
+    private Button btnStaff;
+    private Button btnOrders;
+    private Button btnSupport;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+
+        btnBook = findViewById(R.id.btn_book);
+        btnCustomer = findViewById(R.id.btn_customer);
+        btnReports = findViewById(R.id.btn_reports);
+        btnStaff = findViewById(R.id.btn_staff);
+        btnOrders = findViewById(R.id.btn_orders);
+        btnSupport = findViewById(R.id.btn_support);
+
+        btnBook.setOnClickListener(this);
+        btnCustomer.setOnClickListener(this);
+        btnReports.setOnClickListener(this);
+        btnStaff.setOnClickListener(this);
+        btnOrders.setOnClickListener(this);
+        btnSupport.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btn_book:
+                intent = new Intent(Main_Screen.this, Book_List.class);
+                startActivity(intent);
+                break;
+            /*case R.id.btn_customer:
+                intent = new Intent(MainActivity.this, KhachActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_reports:
+                intent = new Intent(MainActivity.this, ReportActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_staff:
+                intent = new Intent(MainActivity.this, NVActivity.class);
+                startActivity(intent);
+                break;*/
+            case R.id.btn_orders:
+                intent = new Intent(Main_Screen.this, Order_screen.class);
+                startActivity(intent);
+                break;
+            /*case R.id.btn_support:
+                intent = new Intent(MainActivity.this, HTActivity.class);
+                startActivity(intent);
+                break;*/
+        }
     }
 }
