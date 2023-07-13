1eff31cd812eae9b4edad97539ad698b7da693ea
thanhstar <thanhstar260@gmail.com>
2023-07-14 03:30:07 +0700

    fix pagination start with negative value, truncate pagination

--- bookrental_web/src/components/forms/pricefilterForm/PriceFilterForm.js
+++ bookrental_web/src/components/forms/pricefilterForm/PriceFilterForm.js
@@ -1,51 +1,54 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './pricefilterform.style.css';
 
 const PriceFilterForm = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [priceFrom, setPriceFrom] = useState('');
-    const [priceTo, setPriceTo] = useState('');
+  const [priceFrom, setPriceFrom] = useState('');
+  const [priceTo, setPriceTo] = useState('');
 
-    const handlePriceFromChange = (e) => {
-        setPriceFrom(e.target.value);
-    };
+  const handlePriceFromChange = (e) => {
+    const value = parseInt(e.target.value);
+    setPriceFrom(value >= 0 ? value : '');
+  };
 
-    const handlePriceToChange = (e) => {
-        setPriceTo(e.target.value);
-    };
+  const handlePriceToChange = (e) => {
+    const value = parseInt(e.target.value);
+    setPriceTo(value >= 0 ? value : '');
+  };
 
-    const redirectToPriceFilterResult = (e) => {
-        if (e.key === 'Enter') {
-          navigate(`/books/price-filter?priceFrom=${priceFrom}&priceTo=${priceTo}`);
-        }
-    };
+  const redirectToPriceFilterResult = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // Prevent form submission
+      navigate(`/books/price-filter?priceFrom=${priceFrom}&priceTo=${priceTo}`);
+    }
+  };
 
-    return (
-        <div>
-        <p>Price</p>
-        <div className='price-input'>
-            <input 
-            type="number" 
-            className='filter-input' 
-            placeholder="From" 
-            value={priceFrom}
-            onChange={handlePriceFromChange}
-            onKeyDown={redirectToPriceFilterResult}
-            />
-            <p>-</p>
-            <input 
-            type="number" 
-            className='filter-input' 
-            placeholder="To" 
-            value={priceTo}
-            onChange={handlePriceToChange}
-            onKeyDown={redirectToPriceFilterResult}
-            />
-        </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <p>Price</p>
+      <div className="price-input">
+        <input
+          type="number"
+          className="filter-input"
+          placeholder="From"
+          value={priceFrom}
+          onChange={handlePriceFromChange}
+          onKeyDown={redirectToPriceFilterResult}
+        />
+        <p>-</p>
+        <input
+          type="number"
+          className="filter-input"
+          placeholder="To"
+          value={priceTo}
+          onChange={handlePriceToChange}
+          onKeyDown={redirectToPriceFilterResult}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default PriceFilterForm;
--- bookrental_web/src/components/layouts/productlistingall/ProductListingAll.js
+++ bookrental_web/src/components/layouts/productlistingall/ProductListingAll.js
@@ -1,58 +1,144 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './productlistingall.styles.css';
 import ProductListingCard from '../../cards/product-listing-card/ProductListingCard';
 import { BookData } from '../../../util/BookData';
 import { Button } from '@chakra-ui/react';
-import { color } from 'framer-motion';
+import { ReactComponent as Right } from "../../../assets/images/right_icon.svg";
+import { ReactComponent as Left } from "../../../assets/images/left_icon.svg";
 
 const ProductListingAll = ({ booksData }) => {
-    const [currentPage, setCurrentPage] = useState(1)
-	const booksPerPage = 12
+  console.log(booksData)
+  const [currentPage, setCurrentPage] = useState(1);
+  const booksPerPage = 12;
+  let pageRangeDisplayed = 3; // Số trang sẽ hiển thị xung quanh trang hiện tại
+  const totalPages = Math.ceil(booksData.length / booksPerPage);
 
-    const indexOfLastBook = currentPage * booksPerPage
-	const indexOfFirstBook = indexOfLastBook - booksPerPage
-	const currentBooks = booksData.slice(indexOfFirstBook, indexOfLastBook)
+  if (totalPages === 2) {
+    pageRangeDisplayed = 2;
+  } else if (totalPages === 1) {
+    pageRangeDisplayed = 1;
+  }
 
-	const handlePageChange = pageNumber => {
-		setCurrentPage(pageNumber)
-	}
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-	const totalPages = Math.ceil(booksData.length / booksPerPage)
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    const halfRange = Math.floor(pageRangeDisplayed / 2);
+    let startPage = currentPage - halfRange;
+    let endPage = currentPage + halfRange;
 
-    return (
-        <section>
-            <div className='product-listing-all-container'>
-                <div className='container'>
-                    <div className="grid-container">
-                        {currentBooks.map((book) => {
-                            return(
-                                <div className="grid-item">
-                                    <ProductListingCard bookData={book}/>
-                                </div>
-                            )
-                        })}
-                        
-                    </div>
-                </div>
-            </div>
-            <div className='page-turn'>
-				{Array.from({ length: totalPages }, (_, i) => i + 1).map(
-					pageNumber => (
-						<Button
-							key={pageNumber}
-							onClick={() => handlePageChange(pageNumber)}
-                            style={
-                                currentPage === pageNumber ? {background: 'var(--primary-color-bright)', color: '#fff'}:{background: '#eee', color: '#000'}
-                            }           
-						>
-							{pageNumber}
-						</Button>
-					)
-				)}
-			</div>
-        </section>
-        
-    )
-}
+    if (startPage <= 0) {
+      endPage += Math.abs(startPage) + 1;
+      startPage = 1;
+    }
+
+    if (endPage > totalPages) {
+      startPage = 1;
+      endPage = totalPages;
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const indexOfLastBook = currentPage * booksPerPage;
+  const indexOfFirstBook = indexOfLastBook - booksPerPage + 1;
+  const currentBooks = booksData.slice(indexOfFirstBook - 1, indexOfLastBook);
+
+  const [isLeftHovered, setLeftHovered] = useState(false);
+  const [isRightHovered, setRightHovered] = useState(false);
+
+  const handleLeftMouseEnter = () => {
+    setLeftHovered(true);
+  };
+
+  const handleLeftMouseLeave = () => {
+    setLeftHovered(false);
+  };
+
+  const handleRightMouseEnter = () => {
+    setRightHovered(true);
+  };
+
+  const handleRightMouseLeave = () => {
+    setRightHovered(false);
+  };
+
+  return (
+    <section>
+      <div className="product-listing-all-container">
+        <div className="container">
+          <div className="grid-container">
+            {currentBooks.map((book) => (
+              <div className="grid-item" key={book.id}>
+                <ProductListingCard bookData={book} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="page-turn">
+        {currentPage > 1 && (
+          <Left
+            onClick={handlePreviousPage}
+            style={{
+              cursor: 'pointer',
+              fill: isLeftHovered ? 'var(--primary-color-bright)' : '#ccc',
+            }}
+            onMouseEnter={handleLeftMouseEnter}
+            onMouseLeave={handleLeftMouseLeave}
+          >
+            Previous
+          </Left>
+        )}
+        {getPageNumbers().map((pageNumber) => (
+          <Button
+            key={pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
+            style={
+              currentPage === pageNumber
+                ? {
+                    background: 'var(--primary-color-bright)',
+                    color: '#fff',
+                  }
+                : { background: '#eee', color: '#000' }
+            }
+          >
+            {pageNumber}
+          </Button>
+        ))}
+        {currentPage < totalPages && (
+          <Right
+            onClick={handleNextPage}
+            style={{
+              cursor: 'pointer',
+              fill: isRightHovered ? 'var(--primary-color-bright)' : '#ccc',
+            }}
+            onMouseEnter={handleRightMouseEnter}
+            onMouseLeave={handleRightMouseLeave}
+          >
+            Next
+          </Right>
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default ProductListingAll;
